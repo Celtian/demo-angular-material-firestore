adead9dc97df4428b96e9eb0522f05b381d1901d
Dominik Hladík <dominik.hladik@seznam.cz>
2024-01-06 22:25:53 +0100

    fix(aria): aria labels fixed

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -8,6 +8,7 @@ import {
   OnDestroy,
   OnInit,
   ViewChild,
+  effect,
   inject,
   signal,
 } from '@angular/core';
@@ -25,7 +26,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { LocalizeRouterModule } from '@gilsdav/ngx-translate-router';
 import { TranslateModule } from '@ngx-translate/core';
-import { combineLatest, debounceTime, first, switchMap } from 'rxjs';
+import { combineLatest, debounceTime } from 'rxjs';
 import { ROUTE_DEFINITION } from 'src/app/shared/constants/route-definition.constant';
 import { PostDeleteDirective } from 'src/app/shared/directives/post-delete.directive';
 import { PostDto } from 'src/app/shared/dto/post.dto';
@@ -75,7 +76,13 @@ export class PostListComponent implements OnInit, OnDestroy {
   public readonly pageSizeOptions = [5, 10, 25, 100];
   public data = signal<PostDto[]>([]);
   public totalCount = signal(0);
-  public query = '';
+
+  public query = signal('');
+  public pageSize = signal(5);
+  public pageIndex = signal(1);
+  public sortBy = signal<keyof PostDto>('id');
+  public sortDirection = signal<'asc' | 'desc'>('asc');
+
   public expandedElement: PostDto | null = null;
   private destroyRef = inject(DestroyRef);
 
@@ -85,7 +92,23 @@ export class PostListComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
     private breadcrumbsPortalService: BreadcrumbsPortalService,
-  ) {}
+  ) {
+    effect(() => {
+      this.apiService
+        .list({
+          page: this.pageIndex(),
+          limit: this.pageSize(),
+          sort: this.sortBy(),
+          order: this.sortDirection(),
+          query: this.query(),
+        })
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((posts) => {
+          this.data.set(posts.items);
+          this.totalCount.set(posts.totalCount);
+        });
+    });
+  }
 
   public ngOnDestroy(): void {
     this.portalContent?.detach();
@@ -97,32 +120,18 @@ export class PostListComponent implements OnInit, OnDestroy {
       this.cdr.detectChanges();
     });
 
-    this.route.queryParamMap.pipe(first(), getParamQuery()).subscribe((query) => {
-      this.query = query;
-      this.cdr.markForCheck();
-    });
-
     combineLatest([
       this.route.queryParamMap.pipe(getParamPage()),
       this.route.queryParamMap.pipe(getParamQuery()),
       this.route.queryParamMap.pipe(getParamSort()),
     ])
-      .pipe(
-        debounceTime(500),
-        switchMap(([page, query, sort]) => {
-          return this.apiService.list({
-            page: page.pageIndex,
-            limit: page.pageSize,
-            sort: sort.sortBy,
-            order: sort.sortDirection,
-            query,
-          });
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe((posts) => {
-        this.data.set(posts.items);
-        this.totalCount.set(posts.totalCount);
+      .pipe(debounceTime(1000), takeUntilDestroyed(this.destroyRef))
+      .subscribe(([page, query, sort]) => {
+        this.query.set(query);
+        this.pageIndex.set(page.pageIndex || 1);
+        this.pageSize.set(page.pageSize || 5);
+        this.sortBy.set(sort.sortBy || 'id');
+        this.sortDirection.set(sort.sortDirection || 'asc');
       });
   }
 
@@ -135,8 +144,12 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   public onPageChange(event: PageEvent): void {
+    let pageIndex = null;
+    if (event.pageSize === this.pageSize()) {
+      pageIndex = event.pageIndex + 1 > 1 ? event.pageIndex + 1 : null;
+    }
     this.setFiltersToRoute({
-      pageIndex: event.pageIndex > 0 ? event.pageIndex : null,
+      pageIndex,
       pageSize: event.pageSize,
     });
   }
@@ -150,7 +163,6 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   public onQueryRemove(): void {
-    this.query = '';
     this.setFiltersToRoute({
       query: null,
       pageIndex: null,
@@ -158,7 +170,6 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   public onClear(): void {
-    this.query = '';
     this.setFiltersToRoute({
       query: null,
       pageIndex: null,
